d9ba37744bf3e8a4de6ee63be534bafe458d2e8a
Ilkarion <Myfirststepsprogramming@gmail.com>
2025-09-15 20:58:09 +0300

    added api requests
    GET, PATCH, POST, DELETE

---
+++ server.js
@@ -0,0 +1,59 @@
+import e from "express";
+import { todos } from "./starterArray.js";
+
+const app = e();
+const PORT = 1234;
+
+// ✅ Нужно стоять до всех маршрутов
+app.use(e.json());
+
+let arrayTodo = todos;
+
+app.get("/api/todos", (req, res) => {
+  res.status(200).json(arrayTodo);
+});
+
+
+app.post("/api/todos/add", (req, res) => {
+  const newTodo = {
+    nameTask: req.body.name,  
+    description: req.body.desc,
+    status: req.body.status      
+  };
+  arrayTodo.push(newTodo);
+  res.status(201).json(arrayTodo);
+});
+
+app.patch("/api/todos/change", (req, res) => {
+    const nameTodo = req.body.name.toLowerCase()
+    const newStatus = req.body.newStatus
+
+    const task = arrayTodo.find((elem) => nameTodo === elem.nameTask)
+
+    if(!task) res.status(404).json({"message": "Todo with this name not found"})
+    
+    task.status = newStatus
+    console.log(task)
+    res.status(200).json(arrayTodo)
+
+})
+
+app.delete("/api/todos/delete", (req, res) => {
+  const nameTodo = req.body.name?.trim().toLowerCase();
+
+  const index = arrayTodo.findIndex(
+    (elem) => elem.nameTask.trim().toLowerCase() === nameTodo
+  );
+  if (index === -1) {
+    return res.status(404).json({
+      message: "Todo with this name not found",
+    });
+  }
+  arrayTodo.splice(index, 1); // удаляем задачу
+  res.status(200).json(arrayTodo);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ starterArray.js
@@ -0,0 +1,32 @@
+export const todos = [
+    {
+        nameTask: 'sleep 8 hours',
+        describtionTask: 'Your goal is to sleep minimum 8 hours all week',
+        status: 'In progress',
+    },
+    {
+        nameTask: 'Read book',
+        describtionTask: 'Your goal is to read book which have min: 200pages',
+        status: 'false'
+    },
+    {
+        nameTask: 'Trip',
+        describtionTask: 'Your goal is to travel to another city/coutry and see smth beautifull in nature.',
+        status: 'false'
+    },
+    {
+        nameTask: 'Photo',
+        describtionTask: 'Take photo with your family',
+        status: 'completed'
+    },
+    {
+        nameTask: 'Friend',
+        describtionTask: 'Your goal is to find really good friend. Which will have same goals as you. And he will likes you and you will like him',
+        status: 'false'
+    },
+    {
+        nameTask: 'Present',
+        describtionTask: 'Make present for your father and mother',
+        status: 'In progress'
+    },
+    ]
